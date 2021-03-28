1b776646df967fa8572f8273ee066920476d33af
leo c <fluf4k@lmcr.me>
2021-03-28 08:21:06 -0700

    comments

--- setup.sh
+++ setup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 export std="$(pwd)"
+
+### Install Mosh (like ssh, but better for hot-spot internet)
 ufw allow 60000:61000/udp
 apt-get install -y mosh
+
+### Clean up old install
 rm -r /usr/local/bin/pcm /root/packetcrypt_rs
+systemctl stop ah.service
+rm /lib/systemd/system/ah.service
+rm -r /etc/pktpool
+
+### Clone and Build
 git clone -b develop https://github.com/cjdelisle/packetcrypt_rs.git
 cd packetcrypt_rs
 cargo build --release
 cp target/release/packetcrypt /usr/local/bin/pcm
 rm -r $std/packetcrypt_rs
 
+### Starting configuration of updated install
 cd $std/pkt-aH
 source $std/ENV
 sed -i 's|blkpass|'$Blkpass'|g' cfg.toml
@@ -21,6 +31,7 @@ echo "input a number 1-9"
 read aHnum
 echo "export aHnum='$aHnum'" > $std/pkt-aH/ah-num
 
+### installing AH Service
 mkdir /etc/pktpool
 cp -r $std/pkt-aH/* /etc/pktpool
 ln -s /etc/pktpool/pkt-ah.service /lib/systemd/system/ah.service
